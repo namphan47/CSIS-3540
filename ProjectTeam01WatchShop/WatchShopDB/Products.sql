57cf4840b4ee91c2941e3e5e5b29252f66fa17cf
ronakparasrampuria <52551554+ronakparasrampuria@users.noreply.github.com>
2020-03-21 22:46:09 -0700

    Project Deleted

--- ProjectTeam01WatchShop/WatchShopDB/Products.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Products]
-(
-	[ProductID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [ProductBrand] VARCHAR(50) NOT NULL, 
-    [ProductDescription] VARCHAR(50) NULL, 
-    [ProductQuantity] INT NULL, 
-    [ProductPrice] INT NULL, 
-    [ProductImage] VARCHAR(50) NOT NULL
-)
