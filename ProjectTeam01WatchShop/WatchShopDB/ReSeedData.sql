c9c7555e0b7bff31e8736779b7a429f941a0ea69
nam <nphan@atomiton.com>
2020-03-30 21:15:23 -0700

    update

---
+++ ProjectTeam01WatchShop/WatchShopDB/ReSeedData.sql
@@ -0,0 +1,20 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+use WatchShopDB;
+
+go 
+
+INSERT INTO Roles([Type]) VALUES('Admin');
+INSERT INTO Roles([Type]) VALUES('Sale1');
+INSERT INTO Roles([Type]) VALUES('Manager');
+
+go
